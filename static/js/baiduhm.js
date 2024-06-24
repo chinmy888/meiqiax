1c965aa7c48341b2fa80a26045b7f3abe2ef6dc7
chinmy888 <411191866@qq.com>
2024-06-24 16:38:18 +0800

    Add files via upload

---
+++ static/js/baiduhm.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?749a9b99a1c14a45712efed8c3b8fedd";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ static/js/default.js
@@ -0,0 +1,27 @@
+(function(a, b, c, d, e, j, s) {
+  a[d] = a[d] || function() {
+    (a[d].a = a[d].a || []).push(arguments)
+  };
+  j = b.createElement(c),
+      s = b.getElementsByTagName(c)[0];
+  j.async = true;
+  j.charset = 'UTF-8';
+  j.src = 'https://static.meiqia.com/widget/loader.js';
+  s.parentNode.insertBefore(j, s);
+})(window, document, 'script', '_MEIQIA');
+_MEIQIA('entId', 'b269235deb4454abab2f0e543d39725b');
+
+if (location.href.includes('privatization')) {
+  _MEIQIA('subSource', 'sub_source_40');
+}
+if (location.href.includes('industry')) {
+  _MEIQIA('subSource', 'sub_source_45');
+}
+
+if (location.href.includes('ai-bot')) {
+  _MEIQIA('subSource', 'sub_source_58');
+}
+
+window.widgetLoadTime = Date.now()
+
+// fa417e72be909a80fa891c6f66cd6752
